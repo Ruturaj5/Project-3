83789c1ba289288c49d7738e2db655121ddfbef9
rutur <rutur@RUTURAJ>
2024-01-11 12:56:02 +0530

    BY Ruturaj

---
+++ Git Program 3rd PROJECT 11 January/src/com/cjc/Demo3/Git_Demo3.java
@@ -0,0 +1,24 @@
+package com.cjc.Demo3;
+
+public class Git_Demo3 
+{   
+	public void m2()
+	{
+	System.out.println("Method m2");
+	
+	}
+
+	public void m3() 
+	{
+	System.out.println("Methos m3");
+	}
+
+
+	public static void main(String[] args) 
+	{
+		Git_Demo3 g3 = new Git_Demo3();
+		g3.m2();
+		g3.m3();
+	}
+
+}
